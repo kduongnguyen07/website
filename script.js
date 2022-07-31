d4babad9a968b7b9dcfa009105153b88271f815a
kduongnguyen07 <100816706+kduongnguyen07@users.noreply.github.com>
2022-07-31 14:22:45 +0700

    Music!!!!

--- script.js
+++ script.js
@@ -16,4 +16,21 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+});
+//testing for website music
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
+});
