5f4a7b8927c6fc3670e215ac56e7c59213d1013a
kduongnguyen07 <100816706+kduongnguyen07@users.noreply.github.com>
2022-09-04 15:29:54 +0700

    thay tí nhạc cho đổi không khí nha

--- script.js
+++ script.js
@@ -35,3 +35,10 @@ button.addEventListener("click", () => {
   }
   button.classList.add("fade");
 });
+/*Scrool*/
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
